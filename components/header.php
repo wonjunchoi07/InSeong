07dfe77d2e7d07c11160407003f7c50f21cd99d4
manarana <chossun04@gmail.com>
2024-02-23 13:25:09 +0900

    헤더 수정및 라우터 수정

--- components/header.php
+++ components/header.php
@@ -13,16 +13,30 @@
         </div>
         <div id="right">
             <ul>
-                <?php
-                if(isset($_SESSION["user_idx"])){
-                    echo "<li><a href='./logout'>logout</a></li>";
+            <?php
+            if(isset($_SESSION["user_idx"])) {
+                if($_SESSION["is_admin"] == 1) {
+                    echo "
+                    <li><a href='./posting'>게시글 작성</a></li>
+                    <li><a href='./posts'>게시글 보기</a></li>
+                    <li><a href='./admin'>관리자 페이지</a></li>
+                    <li><a href='./logout'>로그아웃</a></li>
+                    ";
                 }else{
                     echo "
-                    <li><a href='./dir.php'>로그인</a></li>
-                    <li><a href='./admin.php'>회원가입</a></li>";
+                    <li><a href='./posting'>게시글 작성</a></li>
+                    <li><a href='./posts'>게시글 보기</a></li>
+                    <li><a href='./logout'>로그아웃</a></li>
+                    ";
                 }
-                    
-                ?>
+            } else {
+                echo "
+                <li><a href='./posts'>게시글 보기</a></li>
+                <li><a href='./login'>로그인</a></li>
+                <li><a href='./register'>회원가입</a></li>
+                ";
+            }
+            ?>
             </ul>
         </div>
     </header>
